6728f39a6b1fd5e8534d3bc4f983eae61ba5f3e9
mohammed.samour.pr <mohammed.samour.pr@gmail.com>
2023-10-03 10:36:28 +0300

    new add3

--- src/test/java/exTest.java
+++ src/test/java/exTest.java
@@ -1,24 +1,28 @@
+import org.jetbrains.annotations.NotNull;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class exTest {
 
-    @Test(priority = 1)
-    public void print() {
-        System.out.println("Test ...");
-    }
 
     @BeforeMethod
-    public void beforeTest() {
-        System.out.println("Before Method");
+    public void printtStatus(ITestResult iTestResult) {
+        // System.out.println("Before Method");
+        System.out.println(iTestResult.getMethod().getMethodName() + "Started.....");
     }
 
-
     @AfterMethod
-    public void afterTest() {
-        System.out.println("After Method");
+    public void printStatus(ITestResult iTestResult) {
+        // System.out.println("After Method");
+        System.out.println(iTestResult.getMethod().getMethodName() + "Finished....");
+    }
+
+    @Test(priority = 1)
+    public void print() {
+        System.out.println("Test...!!");
     }
 
 
