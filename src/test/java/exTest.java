82c195c91eda1a8ce6fa3d308b72a1bee438ba1d
mohammed.samour.pr <mohammed.samour.pr@gmail.com>
2023-10-03 10:20:55 +0300

    new add2

--- src/test/java/exTest.java
+++ src/test/java/exTest.java
@@ -1,11 +1,25 @@
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class exTest {
 
     @Test(priority = 1)
-    public void Test() {
-        Assert.assertTrue(1 == 1);
+    public void print() {
+        System.out.println("Test ...");
     }
+
+    @BeforeMethod
+    public void beforeTest() {
+        System.out.println("Before Method");
+    }
+
+
+    @AfterMethod
+    public void afterTest() {
+        System.out.println("After Method");
+    }
+
 
 }
