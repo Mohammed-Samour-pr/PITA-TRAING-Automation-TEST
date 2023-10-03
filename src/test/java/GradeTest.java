b89427f4e32cacd222810facf8db28a664c16843
mohammed.samour.pr <mohammed.samour.pr@gmail.com>
2023-10-03 11:34:10 +0300

    new add

---
+++ src/test/java/GradeTest.java
@@ -0,0 +1,21 @@
+import jdk.jfr.Description;
+import org.example.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class GradeTest {
+    @DataProvider(name = "gradeData")
+    public Object[][] data() {
+        return new Object[][]{{100, "Excellent"}, {-1, "Invalid"},};
+    }
+
+
+    @Test(dataProvider = "gradeData")
+    public void testGrade(int mark, String expected) {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(calculator.getGrade(mark), expected);
+    }
+
+
+}
